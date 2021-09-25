a71d35dbe44bb17537b62f771a780d5d92f1dd3c
reihiraga <reihiraga@users.noreply.github.com>
2021-09-26 03:44:21 +0700

    PHP REST-API

--- PHP/connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$host ="localhost"; //database hostname
-	$user ="root"; //database User ID
-	$pass =""; //database password
-	$database ="iot_ts";//database name
-	$connect=mysqli_connect($host,$user,$pass,$database) or die 
-	("Connection Failed"); 
-?>
